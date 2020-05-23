97ad1a92b37850ed3bb4a2e17daf8bbb8f3b600c
time <time@times-iMac.local>
2020-05-23 11:37:33 +0800

    debug

--- src/express.php
+++ src/express.php
@@ -3,7 +3,7 @@ namespace ZJTmkj;
 include "KdApiSearchDemo.php";
 class express
 { 
-	public function get($express_code , $express_no)
+	public static function get($express_code , $express_no)
 	{
 		return getOrderTracesByJson($express_code,$express_no);
 	} 
