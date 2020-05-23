38fc53ab2a5364e8dd332b151cc87a2556ad687a
time <time@times-iMac.local>
2020-05-23 14:42:51 +0800

    debug

--- src/express.php
+++ src/express.php
@@ -3,6 +3,10 @@ namespace zjtmkj;
 include "KdApiSearchDemo.php";
 class express
 { 
+    /*
+     *@parmas 快递代码
+     *@params 快递单号
+     */
 	public static function get($express_code , $express_no)
 	{
 		return getOrderTracesByJson($express_code,$express_no);
