ba8eaedeb189aaff5d049698b14bf911cf8ddbab
time <time@times-iMac.local>
2020-05-23 14:46:06 +0800

    debug

---
+++ src/test/index.php
@@ -0,0 +1,8 @@
+<?php
+ 
+// public function express_test(){
+//     $ShipperCode = "STO";
+//     $LogisticCode = ""; 
+//    $result =  express::get($ShipperCode,$LogisticCode);
+//    return response()->json(\Qiniu\json_decode($result));
+// }
